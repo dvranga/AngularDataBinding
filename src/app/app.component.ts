658074160ecae02e29017a19889dfa21b861b466
dvranga <dvranga@users.noreply.github.com>
2020-12-26 05:08:02 +0530

    [ranganath]Added:One way interpolation data binding using template expression to display the hello wrold message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello from Ranganath Vatti';
+  title = 'HelloWorld';
+
+  ngOnInit():void{
+    this.title="Hello from Ranganath Vatti. ";
+  }
 }
