b891fcccc18b62d805d891fd0ebab173c7ec8bcc
dvranga <dvranga@users.noreply.github.com>
2020-12-26 06:59:54 +0530

    [ranganath]Added:Event data binding to display valid input consisting of initial caps and minimum 3 letters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl;
   url;
   userName:string = "";
+  nameError:string="";
 
   ngOnInit():void{
     this.title="Hello from Bridgelabz. ";
@@ -20,5 +21,15 @@ export class AppComponent {
   onClick($event){
     console.log(" logo is clincked ",$event);
     window.open(this.url, "_blank")
+  }
+
+  onInput($event){
+    console.log("Change event occured ",$event);
+    const nameRegex=RegExp('^[A-Z]{1,}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect ";
   }
 }
